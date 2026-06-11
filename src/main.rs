8fe9fb272a74fc03594ed6ae8d277c653d707cf3
binerblob <vivien.hjkl@gmail.com>
2026-06-11 12:24:27 +0700

    refactor ugly code

--- src/main.rs
+++ src/main.rs
@@ -1,99 +1,53 @@
-use std::process::Command;
-use clap::Parser;
+use tokio::process::Command;
+use clap::{ArgAction::Append, Parser};
 use futures::stream::StreamExt;
-use upower_dbus::DeviceProxy;
+use upower_dbus::{BatteryState, DeviceProxy};
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
-	#[arg(long = "wait", num_args = 2, action = clap::ArgAction::Append)]
-    raw_battery_commands: Vec<String>,
+	#[arg(long, default_value = "/org/freedesktop/UPower/devices/battery_BAT0")]
+	device_path: String,
+	
+	#[arg(long = "wait", action = Append, required = true)]
+    percentage: Vec<u8>,
+
+	#[arg(required = true)]
+	command: Vec<String>
 }
 
-fn shell(cmd_str: String) {
-	Command::new("sh")
-		.arg("-c")
-		.arg(cmd_str)
-		.output()
-		.unwrap();
-}
+#[tokio::main]
+async fn main() -> zbus::Result<()> {
+	let args = Args::parse();
 
-struct BatteryInstance {
-	connection: zbus::Connection,
-	battery_path: String,
-	device: DeviceProxy<'static>
-}
+	let connection = zbus::Connection::system().await?;
+	let battery_path = args.device_path;
+	let device = DeviceProxy::new(&connection, battery_path.clone()).await?;
+	
+	let percentage_stream = device.receive_percentage_changed().await;
 
-impl BatteryInstance {
-	async fn new() -> Result<Self, zbus::Error> {
-		let connection = zbus::Connection::system().await?;
-		let battery_path = String::from("/org/freedesktop/UPower/devices/battery_BAT0");
-		let device = DeviceProxy::new(&connection, battery_path.clone()).await?;
+	tokio::pin!(percentage_stream);
 
-		Ok(Self {
-			connection,
-			battery_path,
-			device
-		})
-	}
-}
-
-struct BatteryCommand {
-	percentage: u8,
-	command: String,
-}
-
-struct BatteryCommands {
-	list: Vec<BatteryCommand>
-}
-
-impl BatteryCommands {
-	fn from_args(raw_commands: Vec<String>) -> Self {
-		let mut list = Vec::new();
-		for chunk in raw_commands.chunks_exact(2) {
-			let current_percentage = chunk[0]
-				.parse::<u8>()
-				.expect("count must be a number");
-			let trigger_command = chunk[1].clone();
-
-			list.push(BatteryCommand {
-				percentage: current_percentage,
-				command: trigger_command
-			});
+	while let Some(event) = percentage_stream.next().await {
+		// Skips the follow blocks if not discharging
+		let battery_state = device.state().await;
+		if battery_state.ok() != Some(BatteryState::Discharging) {
+			continue;
 		}
-		Self { list }
-	}
-}
 
-fn main() -> zbus::Result<()> {
-    futures::executor::block_on(async move {
-		let args = Args::parse();
-
-		let battery_instance = BatteryInstance::new().await?;
+		let current_percentage = event.get().await.unwrap() as u8;
 		
-		let mut percentage_stream =
-			battery_instance.device.receive_percentage_changed().await;
-
-		let battery_commands =
-			BatteryCommands::from_args(args.raw_battery_commands);
-
-		while let Some(event) = percentage_stream.next().await {
-			println!("battery percentage event received:\n");
-			let current_percentage = event.get().await.unwrap() as u8;
-
-			for entry in &battery_commands.list {
-				println!("parsed percentage: {}\ncurrent percentage: {}\n",
-						 entry.percentage,
-						 current_percentage
-				);
-				if current_percentage == entry.percentage {
-					shell(String::from(&entry.command));
-					println!("running command: \"{}\"\n", entry.command);
-				} else {
-					println!("not running command: \"{}\"\n", entry.command);
-				};
+		for (arg_percentage, arg_command) in args.percentage.iter().zip(&args.command) {
+			if &current_percentage == arg_percentage {
+				// shell(arg_command.to_string());
+				Command::new("sh")
+					.arg("-c")
+					.arg(arg_command)
+					.spawn()?
+					.wait()
+					.await?;
 			}
 		}
-        Ok(())
-    })
+	}
+    Ok(())
 }
